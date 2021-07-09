1da7d2912f6573b3aa71d27c30de94b01a4eceff
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-07-09 19:18:13 +0700

    feat(gatsby): PQR: merge data dependencies from workers to the main process

--- packages/gatsby/src/utils/worker/pool.ts
+++ packages/gatsby/src/utils/worker/pool.ts
@@ -7,6 +7,8 @@ import { IGroupedQueryIds } from "../../services"
 import { initJobsMessagingInMainProcess } from "../jobs/worker-messaging"
 
 import { GatsbyWorkerPool } from "./types"
+import { loadPartialStateFromDisk, store } from "../../redux"
+import { IGatsbyState, IMergeWorkerQueryState } from "../../redux/types"
 
 export type { GatsbyWorkerPool }
 
@@ -63,6 +65,25 @@ export async function runQueriesInWorkersQueue(
   }
 
   await Promise.all(promises)
+  await mergeWorkerState(pool)
 
   activity.end()
+}
+
+async function mergeWorkerState(pool: GatsbyWorkerPool): Promise<void> {
+  await pool.all.saveQueries()
+  const workerQueryState: IMergeWorkerQueryState["payload"] = []
+
+  for (const { workerId } of pool.getWorkerInfo()) {
+    const state = loadPartialStateFromDisk([`queries`], String(workerId))
+    workerQueryState.push({
+      workerId,
+      queryStateChunk: state as IGatsbyState["queries"],
+    })
+    await new Promise(resolve => process.nextTick(resolve))
+  }
+  store.dispatch({
+    type: `MERGE_WORKER_QUERY_STATE`,
+    payload: workerQueryState,
+  })
 }
